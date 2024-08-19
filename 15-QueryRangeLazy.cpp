c213bf55137d957412f88a9ef112ad904ff4fc7a
Hamza Fatnaoui <hamzafatnaoui.ai@gmail.com>
2024-08-20 00:09:12 +0100

    Query Range using Lazy propagation

---
+++ 15-QueryRangeLazy.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+ll lazy[1000] = {0};
+
+void BuildTree(ll *arr,ll s,ll e,ll *tree,ll index){
+    if(s == e){
+        tree[index] = arr[s];
+        return ;
+    }
+    ll middle = (s + e)/2;
+    BuildTree(arr,s,middle,tree,2*index);
+    BuildTree(arr,middle+1,e,tree,2*index+1);
+    tree[index] = min(tree[2*index],tree[2*index+1]);
+    return ;
+}
+
+void UpdateRangeLazy(ll s,ll e,ll l,ll r,ll *tree,ll inc,ll index){
+    if(lazy[index] != 0){
+        tree[index] += lazy[index];
+        if(s != e){
+            lazy[2*index] += lazy[index];
+            lazy[2*index+1] += lazy[index];
+        }
+        lazy[index] = 0;
+    }
+    if((s > r) || (e < l)){
+        return ;
+    }
+    if((l <= s) && (e <= r)){ // optimization
+        tree[index] += inc;
+        if(s != e){
+            lazy[2*index] += inc;
+            lazy[2*index] += inc;
+        }
+        return ;
+    }
+    ll middle =(s + e)/2;
+    UpdateRangeLazy(s,middle,l,r,tree,inc,2*index);
+    UpdateRangeLazy(middle+1,e,l,r,tree,inc,2*index+1);
+    tree[index] = min(tree[2*index],tree[2*index+1]);
+    return ;
+}
+
+ll queryLazy(ll s,ll e,ll l,ll r,ll *tree,ll index){
+    if(lazy[index] != 0){
+        tree[index] += lazy[index];
+        if(s != e){
+            lazy[2*index] += lazy[index];
+            lazy[2*index+1] += lazy[index];
+        }
+        lazy[index] = 0;
+    }
+    if((s > r) || (e < l)){
+        return LLONG_MAX ;
+    }
+    if((l <= s) && (e <= r)){
+        return tree[index];
+    }
+    ll middle =(s + e)/2;
+    ll left = queryLazy(s,middle,l,r,tree,2*index);
+    ll right = queryLazy(middle+1,e,l,r,tree,2*index+1);
+    return min(tree[2*index],tree[2*index+1]);
+}
+
+int main() {
+    ll arr[] = {3,2,5,4,1};
+    ll n = sizeof(arr)/sizeof(ll);
+    ll *tree = new ll[4*n+1];
+    BuildTree(arr,0,n-1,tree,1);
+    UpdateRangeLazy(0,n-1,3,4,tree,3,1);
+    cout<<queryLazy(0,n-1,1,4,tree,1);
+}
